5bd53c20dc27769b5b250464a71180528ff75e46
Steven ongkowidjojo <songkowidjojo@student.ciputra.ac.id>
2022-03-13 20:23:11 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Take_Home_Assignment_Week_3
+{
+    partial class FormPengaturanWarna
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblInput = new System.Windows.Forms.Label();
+            this.TxtBoxInput = new System.Windows.Forms.TextBox();
+            this.BtnProses = new System.Windows.Forms.Button();
+            this.LblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblInput
+            // 
+            this.LblInput.AutoSize = true;
+            this.LblInput.Location = new System.Drawing.Point(12, 9);
+            this.LblInput.Name = "LblInput";
+            this.LblInput.Size = new System.Drawing.Size(105, 25);
+            this.LblInput.TabIndex = 0;
+            this.LblInput.Text = "Input Data :";
+            // 
+            // TxtBoxInput
+            // 
+            this.TxtBoxInput.Location = new System.Drawing.Point(12, 37);
+            this.TxtBoxInput.Multiline = true;
+            this.TxtBoxInput.Name = "TxtBoxInput";
+            this.TxtBoxInput.Size = new System.Drawing.Size(496, 35);
+            this.TxtBoxInput.TabIndex = 1;
+            this.TxtBoxInput.TabStop = false;
+            // 
+            // BtnProses
+            // 
+            this.BtnProses.Location = new System.Drawing.Point(12, 105);
+            this.BtnProses.Name = "BtnProses";
+            this.BtnProses.Size = new System.Drawing.Size(115, 40);
+            this.BtnProses.TabIndex = 2;
+            this.BtnProses.Text = "Proses";
+            this.BtnProses.UseVisualStyleBackColor = true;
+            this.BtnProses.Click += new System.EventHandler(this.BtnProses_Click);
+            // 
+            // LblOutput
+            // 
+            this.LblOutput.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblOutput.Location = new System.Drawing.Point(12, 175);
+            this.LblOutput.Name = "LblOutput";
+            this.LblOutput.Size = new System.Drawing.Size(486, 100);
+            this.LblOutput.TabIndex = 3;
+            this.LblOutput.Text = "[EMPTY]";
+            // 
+            // FormPengaturanWarna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(528, 291);
+            this.Controls.Add(this.LblOutput);
+            this.Controls.Add(this.BtnProses);
+            this.Controls.Add(this.TxtBoxInput);
+            this.Controls.Add(this.LblInput);
+            this.Name = "FormPengaturanWarna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Pengaturan Warna";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormPengaturanWarna_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblInput;
+        private System.Windows.Forms.TextBox TxtBoxInput;
+        private System.Windows.Forms.Button BtnProses;
+        private System.Windows.Forms.Label LblOutput;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Take_Home_Assignment_Week_3
+{
+    public partial class FormPengaturanWarna : Form
+    {
+        public FormPengaturanWarna()
+        {
+            InitializeComponent();
+        }
+        
+        private void BtnProses_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TxtBoxInput.Text.ToUpper() == "UP" || TxtBoxInput.Text.ToUpper() == "BESAR")
+                    LblOutput.Font = new Font("Segoe UI", LblOutput.Font.Size + 1, FontStyle.Bold);
+                else if (TxtBoxInput.Text.ToUpper() == "DOWN" || TxtBoxInput.Text.ToUpper() == "KECIL")
+                    LblOutput.Font = new Font("Segoe UI", LblOutput.Font.Size - 1, FontStyle.Bold);
+                else if (TxtBoxInput.Text.ToUpper() == "RESTART")
+                {
+                    LblOutput.Font = new Font("Segoe UI", 16, FontStyle.Bold);
+                    LblOutput.ForeColor = Color.Black;
+                    LblOutput.Visible = true;
+                    LblOutput.Text = "[EMPTY]";
+                }
+                else if (TxtBoxInput.Text.Substring(0, 4).ToUpper() == "ISI:")
+                    LblOutput.Text = TxtBoxInput.Text.Substring(4);
+                else if (TxtBoxInput.Text.ToUpper() == "SHOWN")
+                    LblOutput.Visible = true;
+                else if (TxtBoxInput.Text.ToUpper() == "HIDE")
+                    LblOutput.Visible = false;
+                else if (TxtBoxInput.Text.Substring(0, 6).ToUpper() == "WARNA:")
+                {
+                    if (TxtBoxInput.Text.Substring(6).ToUpper() == "MERAH" || TxtBoxInput.Text.Substring(6).ToUpper() == "RED")
+                        LblOutput.ForeColor = Color.Red;
+                    else if (TxtBoxInput.Text.Substring(6).ToUpper() == "HIJAU" || TxtBoxInput.Text.Substring(6).ToUpper() == "GREEN")
+                        LblOutput.ForeColor = Color.Green;
+                    else if (TxtBoxInput.Text.Substring(6).ToUpper() == "BIRU" || TxtBoxInput.Text.Substring(6).ToUpper() == "BLUE")
+                        LblOutput.ForeColor = Color.Blue;
+                    else if (TxtBoxInput.Text.Substring(6).ToUpper() == "DEFAULT" || TxtBoxInput.Text.Substring(6).ToUpper() == "BLACK" || TxtBoxInput.Text.Substring(6).ToUpper() == "HITAM")
+                        LblOutput.ForeColor = Color.Black;
+                    else
+                        MessageBox.Show("Syntax tidak terdaftar!");
+                }
+                else
+                    MessageBox.Show("Syntax tidak terdaftar");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Syntax tidak terdaftar");
+            }
+        }
+
+        private void FormPengaturanWarna_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                BtnProses.PerformClick();
+        }
+    }
+}
